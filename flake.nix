5f4a69195fb5f2f65b14d9d5a708a2e7995d7b00
elyrisai <elyrisai@tutamail.com>
2026-07-27 23:07:22 +0600

    zola >> hugo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for the site management";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -18,25 +18,16 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        # Development shell with Zola
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.zola ];
+          packages = with pkgs; [
+            hugo
+            nodejs
+            pnpm
+          ];
 
           shellHook = ''
-            echo "Zola ${pkgs.zola.version} is ready."
-            echo "Ensure 'themes/zerene' exists in your project directory."
-            echo "Run 'zola serve' to start the local server."
+            echo "Hugo version: $(hugo --version)"
           '';
-        };
-
-        # Optional: Package to build the site assuming theme exists locally
-        packages.website = pkgs.stdenv.mkDerivation {
-          pname = "zola-site";
-          version = "0.1.0";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.zola ];
-          buildPhase = "zola build";
-          installPhase = "cp -r public $out";
         };
       }
     );
